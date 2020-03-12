82d85102a11b2dd3b4779d6671a6dc42083f9330
Ferdinand Mom <ferdinand.mom@epita.fr>
2020-03-12 10:16:45 +0100

    add efficientNet transfer learning

---
+++ src/architecture/efficientnet/tensorflow_2/cinic10.py
@@ -0,0 +1,140 @@
+import os
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import misc
+from six.moves import urllib
+import tarfile
+
+
+def loadData(pathToDatasetFolder, oneHot=False):
+    """
+    pathToDatasetFolder: Parent folder of CINIC-10 dataset folder or CINIC-10.tar.gz file
+    oneHot: Label encoding (one hot encoding or not)
+
+    Return: Train, validation and test sets and label numpy arrays
+    """
+    sourceUrl = "https://datashare.is.ed.ac.uk/bitstream/handle/10283/3192/CINIC-10.tar.gz"
+    pathToFile = downloadDataset(pathToDatasetFolder, "CINIC-10.tar.gz", sourceUrl)
+
+    labelDict = {'airplane': 0, 'automobile': 1, 'bird': 2, 'cat': 3,
+                'deer': 4, 'dog': 5, 'frog': 6, 'horse': 7, 'ship': 8,
+                'truck': 9}
+
+    pathToTrain = os.path.join(pathToFile, "train")
+    pathToVal = os.path.join(pathToFile, "valid")
+    pathToTest = os.path.join(pathToFile, "test")
+
+    imgNamesTrain = [f for dp, dn, fn in os.walk(os.path.expanduser(pathToTrain)) for f in fn]
+    imgDirsTrain = [dp for dp, dn, fn in os.walk(os.path.expanduser(pathToTrain)) for f in fn]
+    imgNamesVal = [f for dp, dn, fn in os.walk(os.path.expanduser(pathToVal)) for f in fn]
+    imgDirsVal = [dp for dp, dn, fn in os.walk(os.path.expanduser(pathToVal)) for f in fn]
+    imgNamesTest = [f for dp, dn, fn in os.walk(os.path.expanduser(pathToTest)) for f in fn]
+    imgDirsTest = [dp for dp, dn, fn in os.walk(os.path.expanduser(pathToTest)) for f in fn]
+
+    XTrain = np.empty((len(imgNamesTrain), 32, 32, 3), dtype=np.float32)
+    YTrain = np.empty((len(imgNamesTrain)), dtype=np.int32)
+    XVal = np.empty((len(imgNamesVal), 32, 32, 3), dtype=np.float32)
+    YVal = np.empty((len(imgNamesVal)), dtype=np.int32)
+    XTest = np.empty((len(imgNamesTest), 32, 32, 3), dtype=np.float32)
+    YTest = np.empty((len(imgNamesTest)), dtype=np.int32)
+
+    print("Loading")
+
+    for i in range(len(imgNamesTrain)):
+        # img = plt.imread(os.path.join(imgDirsTrain[i], imgNamesTrain[i]))
+        img = misc.imread(os.path.join(imgDirsTrain[i], imgNamesTrain[i]))
+        if len(img.shape) == 2:
+            XTrain[i, :, :, 2] = XTrain[i, :, :, 1] = XTrain[i, :, :, 0] = img/255.
+        else:
+            XTrain[i] = img/255.
+        YTrain[i] = labelDict[os.path.basename(imgDirsTrain[i])]
+    for i in range(len(imgNamesVal)):
+        # img = plt.imread(os.path.join(imgDirsVal[i], imgNamesVal[i]))
+        img = misc.imread(os.path.join(imgDirsVal[i], imgNamesVal[i]))
+        if len(img.shape) == 2:
+            XVal[i, :, :, 2] = XVal[i, :, :, 1] = XVal[i, :, :, 0] = img/255.
+        else:
+            XVal[i] = img/255.
+        YVal[i] = labelDict[os.path.basename(imgDirsVal[i])]
+    for i in range(len(imgNamesTest)):
+        # img = plt.imread(os.path.join(imgDirsTest[i], imgNamesTest[i]))
+        img = misc.imread(os.path.join(imgDirsTest[i], imgNamesTest[i]))
+        if len(img.shape) == 2:
+            XTest[i, :, :, 2] = XTest[i, :, :, 1] = XTest[i, :, :, 0] = img/255.
+        else:
+            XTest[i] = img/255.
+        YTest[i] = labelDict[os.path.basename(imgDirsTest[i])]
+
+    if oneHot:
+        YTrain = toOneHot(YTrain, 10)
+        YVal = toOneHot(YVal, 10)
+        YTest = toOneHot(YTest, 10)
+
+    print("+ Dataset loaded")
+
+    return XTrain, YTrain, XVal, YVal, XTest, YTest
+
+
+def downloadDataset(dirName, fileName, sourceUrl):
+    """
+    https://github.com/tflearn/tflearn/blob/master/tflearn/datasets/cifar10.py
+    """
+    cinicDirName = os.path.join(dirName, "CINIC-10/")
+    if not os.path.exists(cinicDirName):
+        os.mkdir(cinicDirName)
+        pathToFile = os.path.join(dirName, fileName)
+        if not os.path.exists(pathToFile):
+            print("Downloading")
+            pathToFile, _ = urllib.request.urlretrieve(sourceUrl, pathToFile, reporthook)
+            print("+ Downloaded")
+        untar(pathToFile, cinicDirName)
+    else:
+        print("+ Dataset already downloaded")
+    return cinicDirName
+
+
+def reporthook(blocknum, blocksize, totalsize):
+    """
+    reporthook from stackoverflow #13881092
+    https://github.com/tflearn/tflearn/blob/master/tflearn/datasets/cifar10.py
+    """
+    readsofar = blocknum * blocksize
+    if totalsize > 0:
+        percent = readsofar * 1e2 / totalsize
+        s = "\r%5.1f%% %*d / %d" % (
+            percent, len(str(totalsize)), readsofar, totalsize)
+        sys.stderr.write(s)
+        if readsofar >= totalsize:  # near the end
+            sys.stderr.write("\n")
+    else:  # total size is unknown
+        sys.stderr.write("read %d\n" % (readsofar,))
+
+
+def untar(fname, path):
+    if (fname.endswith("tar.gz")):
+        print("Extracting tar file")
+        tar = tarfile.open(fname)
+        tar.extractall(path=path)
+        tar.close()
+        print("+ Extracted")
+    else:
+        print("Not a tar.gz file")
+
+
+def toOneHot(y, nb_classes=None):
+    """
+    https://github.com/tflearn/tflearn/blob/master/tflearn/data_utils.py#L36
+    """
+    if nb_classes:
+        # y = np.asarray(y, dtype='int32')
+        if len(y.shape) > 2:
+            print("Warning: data array ndim > 2")
+        if len(y.shape) > 1:
+            y = y.reshape(-1)
+        Y = np.zeros((len(y), nb_classes))
+        Y[np.arange(len(y)), y] = 1.
+        return Y
+    else:
+        y = np.array(y)
+        return (y[:, None] == np.unique(y)).astype(np.float32)
--- src/architecture/efficientnet/tensorflow_2/utils.py
+++ src/architecture/efficientnet/tensorflow_2/utils.py
@@ -1,8 +1,157 @@
 import math
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras import layers
+from tensorflow.keras.layers import Input, Dense, Conv2D, DepthwiseConv2D, BatchNormalization, Dropout, GlobalAveragePooling2D, Reshape, multiply, add, Activation
 from skimage.transform import resize
+
+CONV_KERNEL_INITIALIZER = {
+    'class_name': 'VarianceScaling',
+    'config': {
+        'scale': 2.0,
+        'mode': 'fan_out',
+        # EfficientNet actually uses an untruncated normal distribution for
+        # initializing conv layers, but keras.initializers.VarianceScaling use
+        # a truncated distribution.
+        # We decided against a custom initializer for better serializability.
+        'distribution': 'normal'
+    }
+}
+
+DENSE_KERNEL_INITIALIZER = {
+    'class_name': 'VarianceScaling',
+    'config': {
+        'scale': 1. / 3.,
+        'mode': 'fan_out',
+        'distribution': 'uniform'
+    }
+}
+
+def SqueezeExcitation(x, filters_in, filters_expand, se_ratio):
+    """
+        Squeeze and Excitation phase.
+        
+        Parameters:
+        -x: Tensor, input tensor of conv layer.
+        -filters_in: Integer, dimension of the input space.
+        -filters_expand: Integer, Dimension of the output space after expansion
+        -se_ratio: Float, ratio use to squeeze the input filters.
+    """
+    filters_se = max(1, int(filters_in * se_ratio))
+    
+    # Squeeze.
+    se = GlobalAveragePooling2D()(x)
+    se = Reshape((1, 1, filters_expand))(se)
+    
+    # Excitation.
+    se = Conv2D(filters=filters_se,
+                kernel_size=1,
+                padding='same',
+                kernel_initializer= CONV_KERNEL_INITIALIZER,
+                use_bias=True)(se)
+    
+    se = Activation(tf.nn.swish)(se)
+    
+    se = Conv2D(filters=filters_expand,
+                kernel_size=1,
+                padding='same',
+                activation='sigmoid',
+                kernel_initializer= CONV_KERNEL_INITIALIZER,
+                use_bias=True)(se)
+    
+    # Scale.
+    x = multiply([x, se])
+    
+    return x
+
+def __bottleneck(inputs, filters_in, filters_out, kernel_size, expansion_coef, se_ratio, stride, dropout_rate):
+    """
+        Basic bottleneck structure.
+        
+        Parameters:
+        -inputs: Tensor, input tensor of conv layer.
+        -filters_in: Integer, dimension of the input space.
+        -filters_out: Integer, dimension of the output space.
+        -kernel_size: Integer or tuple of 2 integers, width and height of filters.
+        -expansion_coef: Integer, expansion coefficient.
+        -se_ratio: Float, ratio use to squeeze the input filters.
+        -stride: Integer or tuple of 2 integers, conv stride.
+    """
+    # Dimension of the output space after expansion.
+    filters_expand = filters_in * expansion_coef
+    
+    # Expansion phase.
+    if expansion_coef != 1:
+        x = Conv2D(filters=filters_expand,
+                   kernel_size=1,
+                   strides=1,
+                   padding='same',
+                   kernel_initializer=CONV_KERNEL_INITIALIZER,
+                   use_bias=False)(inputs)
+        x = BatchNormalization()(x)
+        x = Activation(tf.nn.swish)(x)
+    else:
+        x = inputs
+    
+    # Dephtwise conv phase.
+    x = DepthwiseConv2D(kernel_size=kernel_size,
+                        strides=stride,
+                        padding='same',
+                        depthwise_initializer=CONV_KERNEL_INITIALIZER,
+                        use_bias=False)(x)
+    x = BatchNormalization()(x)
+    x = Activation(tf.nn.swish)(x)
+    
+    # Squeeze and Excitation phase.
+    x = SqueezeExcitation(x, filters_in, filters_expand, se_ratio)
+    
+    # Output phase.
+    x = Conv2D(filters=filters_out,
+               kernel_size=1,
+               padding='same',
+               kernel_initializer=CONV_KERNEL_INITIALIZER,
+               use_bias=False)(x)
+    
+    x = BatchNormalization()(x)
+    
+    # MobileNetV2 paper: "Add skip connection when stride=1."
+    if (stride == 1 and filters_in == filters_out):
+        if dropout_rate > 0:
+            x = Dropout(dropout_rate)(x)
+        x = add([x, inputs])
+    
+    return x
+
+def MBConvBlock(inputs, filters_in, filters_out, kernel_size, expansion_coef, se_ratio, stride, repeat, dropout_rate):
+    """
+    """
+    x = __bottleneck(inputs, filters_in, filters_out, kernel_size, expansion_coef, se_ratio, stride, dropout_rate)
+    
+    
+    # MobileNetV2 paper: "The first layer of eachsequence has a stride s and all others use stride 1"
+    filters_in = filters_out
+    stride = 1
+    
+    for i in range(1, repeat):
+        x = __bottleneck(x, filters_in, filters_out, kernel_size, expansion_coef, se_ratio, stride, dropout_rate)
+        
+    return x
+
+def ConvBlock(inputs, filters, kernel_size, stride=1, padding='same'):
+    """
+    
+    """ 
+    x = inputs
+    x = Conv2D(filters=filters,
+               kernel_size=kernel_size,
+               strides=stride,
+               padding=padding,
+               use_bias=False,
+               kernel_initializer=CONV_KERNEL_INITIALIZER)(x)
+
+    x = BatchNormalization()(x)
+    x = Activation(tf.nn.swish)(x)
+    
+    return x
 
 def efficientnet_params(model_name):
     """ Map EfficientNet model name to parameter coefficients. """
@@ -70,3 +219,5 @@ def center_crop_and_resize(image, image_size, crop_padding=32, interpolation="bi
     )
 
     return resized_image
+
+
